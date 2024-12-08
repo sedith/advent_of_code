e98db1a6238064d9c5b7874235e5a76f4fc52917
sedith <martin.jacquet@posteo.net>
2024-12-08 19:22:39 +0100

    2024 06 p2 slightly improved

--- 2024/06 - Guard Gallivant/README.md
+++ 2024/06 - Guard Gallivant/README.md
@@ -9,7 +9,7 @@ Increment the guard position until they leave the map.
 
 The concept is to explore a branch by adding an obstacle in front of the guard at each step, and check if this leads to a loop.
 There are several edge cases:
-* check that the obstacle can be placed in front of the guard
+* check that the obstacle can be placed
     * we cannot place the obstacle on the current guard position
     * we cannot place the obstacle on the previously walked path of the guard (imply the previous)
 * don't account for obstacles several times in the same location
@@ -17,3 +17,5 @@ There are several edge cases:
 
 Implementation of path as a dict whose key is the position and value is the list of directions the cell was crossed with.
 The virtual path exploration also creates its own path dict.
+For efficiency, the dict stores only path nodes, ie when there is a change of direction. This avoid building sparser dicts, since the explore function checks appartenance to the lists at each step.
+The full path is stored in a separate list to check that the obstructions.
--- 2024/06 - Guard Gallivant/part_2.py
+++ 2024/06 - Guard Gallivant/part_2.py
@@ -1,6 +1,6 @@
 import time
 from enum import Enum
-from copy import copy, deepcopy
+from copy import deepcopy
 from collections import defaultdict
 
 
@@ -16,6 +16,9 @@ class Dir(Enum):
     def ccw(self):
         return Dir((self.value - 1) % 4)
 
+    def move(self):
+        return ((-1, 0), (0, 1), (1, 0), (0, -1))[self.value]
+
     def __str__(self):
         return ('^', '>', 'v', '<')[self.value]
 
@@ -28,15 +31,9 @@ class Pos(tuple):
         self.grid_size = grid_size
 
     def __add__(self, d):
-        i, j = self
-        if d == Dir.W:
-            j -= 1
-        elif d == Dir.E:
-            j += 1
-        if d == Dir.N:
-            i -= 1
-        elif d == Dir.S:
-            i += 1
+        di, dj = d.move()
+        i = self[0] + di
+        j = self[1] + dj
         if i in [-1, self.grid_size[0]] or j in [-1, self.grid_size[1]]:
             return None
         return Pos(i, j, self.grid_size)
@@ -47,45 +44,50 @@ class Grid:
         self.grid = [[c for c in l] for l in data]
         self.size = (len(data), len(data[0]))
         self.obs = []
-        self.path = defaultdict(list)
+        self.path = []
+        self.nodes = defaultdict(list)
 
     def get(self, pos):
         return self.grid[pos[0]][pos[1]]
 
     def explore_cw_branch(self, pos_init, dir_init):
-        branch_path = defaultdict(list)
+        branch_nodes = defaultdict(list)
         obs_pos = pos_init + dir_init
         dir = dir_init.cw()
         pos = pos_init
         while pos is not None:
-            branch_path[pos].append(dir)
             next_pos = pos + dir
             if next_pos is not None:
                 if next_pos == obs_pos or self.get(next_pos) == '#':
+                    branch_nodes[pos].append(dir)
                     dir = dir.cw()
+                    # print(self.display(pos_init, dir_init, branch_nodes, obs_pos))
+                    # input()
                     continue
-                elif (next_pos in self.path.keys() and dir in self.path[next_pos]) \
-                or (next_pos in branch_path.keys() and dir in branch_path[next_pos]):
+                if (next_pos in self.nodes and dir in self.nodes[next_pos]) or (next_pos in branch_nodes and dir in branch_nodes[next_pos]):
                     self.obs.append(obs_pos)
                     break
             pos = next_pos
 
-    def display(self, pos, dir, path, obstruct):
-        def get_char(pos, path):
-            if len(path[pos]) == 1:
-                return '|' if path[pos][0] in [Dir.N, Dir.S] else '-'
-            elif len(path[pos]) == 2:
-                return '|' if path[pos][0].value % 2 == path[pos][1].value % 2 else '+'
-            else:
-                return '+'
-        grid = deepcopy(self.grid)
-        for pos_path in self.path.keys():
-            grid[pos_path[0]][pos_path[1]] = f'\033[32m{get_char(pos_path, self.path)}\033[0m'  # actual path in green
-        for pos_path in path.keys():
-            grid[pos_path[0]][pos_path[1]] = f'\033[93m{get_char(pos_path, path)}\033[0m'  # hypothetical path in yellow
-        grid[pos[0]][pos[1]] = f'\033[31m{dir}\033[0m'  # current guard position in red
-        grid[obstruct[0]][obstruct[1]] = '\033[31mO\033[0m'  # proposed obstruction in red
-        return ''.join(''.join(line) + '\n' for line in grid)[:-1]
+    # def display(self, pos, dir, path, obstruct):
+    #     def get_char(pos, path):
+    #         if len(path[pos]) == 1:
+    #             return '|' if path[pos][0] in [Dir.N, Dir.S] else '-'
+    #         elif len(path[pos]) == 2:
+    #             return '|' if path[pos][0].value % 2 == path[pos][1].value % 2 else '+'
+    #         else:
+    #             return '+'
+    #
+    #     grid = deepcopy(self.grid)
+    #     for pos_path in self.path:
+    #         grid[pos_path[0]][pos_path[1]] = '\033[32mx\033[0m'  # actual path in green
+    #     for pos_path in self.nodes:
+    #         grid[pos_path[0]][pos_path[1]] = f'\033[32m{get_char(pos_path, self.nodes)}\033[0m'  # actual path in green
+    #     for pos_path in path:
+    #         grid[pos_path[0]][pos_path[1]] = f'\033[93m{get_char(pos_path, path)}\033[0m'  # hypothetical path in yellow
+    #     grid[pos[0]][pos[1]] = f'\033[31m{dir}\033[0m'  # current guard position in red
+    #     grid[obstruct[0]][obstruct[1]] = '\033[31mO\033[0m'  # proposed obstruction in red
+    #     return ''.join(''.join(line) + '\n' for line in grid)[:-1]
 
 
 if __name__ == '__main__':
@@ -110,11 +112,12 @@ if __name__ == '__main__':
     while guard_pos is not None:
         next_pos = guard_pos + dir
         if next_pos is not None:
-            grid.path[guard_pos].append(dir)
+            grid.path.append(guard_pos)
             if grid.get(next_pos) == '#':
+                grid.nodes[guard_pos].append(dir)
                 dir = dir.cw()
                 continue
-            elif next_pos not in grid.obs and next_pos not in grid.path.keys():  # check if we can obstruct the next cell
+            elif next_pos not in grid.obs and next_pos not in grid.path:  # check if we can obstruct the next cell
                 grid.explore_cw_branch(guard_pos, dir)
         guard_pos = next_pos
 
