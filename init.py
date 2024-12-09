56ebbb3af33427cb7b077e80e2410e80626850ed
sedith <martin.jacquet@posteo.net>
2024-12-09 15:50:40 +0100

    update init

--- init.py
+++ init.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
 import os
 import sys
 
 
-template = \
-"""
-
-
-def main(data):
+template_python = \
+"""def main(data):
     return 0
 
 
@@ -24,14 +22,27 @@ if __name__ == '__main__':
     print(f'time [s] : {toc - tic:.5f}')
 """
 
+template_readme = \
+"""# Advent of code %s - Day %s: %s
+
+## Part 1
+
+
+
+## Part 2
+"""
+
 
 if __name__ == '__main__':
-    folder = f'{sys.argv[1]}/{int(sys.argv[2]):02d} - {sys.argv[3]}'
-    os.makedirs(folder)
+    if len(sys.argv) >= 4:
+        year, day, title = sys.argv[1], f'{int(sys.argv[2]):02d}', sys.argv[3]
+        folder = f'{year}/{day} - {title}'
+        os.makedirs(folder)
 
-    open(f'{folder}/input.txt', 'w')
-    open(f'{folder}/example.txt', 'w')
-    open(f'{folder}/README.md', 'w')
-    open(f'{folder}/part2.py', 'w')
-    with open(f'{folder}/part1.py', 'w') as f:
-        f.write(template)
+        open(f'{folder}/input.txt', 'w')
+        open(f'{folder}/example.txt', 'w')
+        open(f'{folder}/part2.py', 'w')
+        with open(f'{folder}/part1.py', 'w') as f:
+            f.write(template_python)
+        with open(f'{folder}/README.md', 'w') as f:
+            f.write(template_readme % (year, day, title))
