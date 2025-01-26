1b96e8690b8baec5b98c07587371eb11bc47e16a
sedith <martin.jacquet@posteo.net>
2025-01-26 17:35:22 +0100

    improve 2024 10

--- 2024/10 - Hoof It/README.md
+++ 2024/10 - Hoof It/README.md
@@ -4,11 +4,9 @@ https://adventofcode.com/2024/day/10
 
 ## Part 1
 
-(Reusing the Dir/Pos implementation for the grid from 2024/04.)
+(Using complex for grip positions again.)
 The grid is scanned for trailheads (`0`s), and for each the trails are explored.
-The data structure is a set of positions for each cells of a given altitude that are in the path.
-We use sets to account for the fact that a position can be reached from several others.
-The lists for levels 1-9 are created iteratively at looking at the 4-neighbors of the position at the level below.
+The exploration performed altitude by altitude. For each cell at a given level, the 4-neighbors of scanned and matching positions (next altitude) are stored in a set (accounting for the fact that a position can be reached in several ways).
 The trail score is the number of cells at level 9.
 
 ## Part 2
@@ -17,4 +15,4 @@ Again, the grid is scanned for trailheads.
 The data structure is a tree-based implementation. Each node of the tree has a list of predecessors and successors and each branch is explored one after the other.
 The rating is computed by recursively exploring each branch and accounting for it if it reaches a summit (`z=9`).
 
-This was actually an attemp at implementing something for part 1, until I realized that I completely disregarding the fact that node count be seen several times. I put this code aside, solved part 1, and realized this was exactly what I needed for part two, so I simply reworded `score` into `rating`.
+The first implementation of part 2 (before switching to complex) was actually an attemp at implementing something for part 1, until I realized that I completely disregarding the fact that node count be seen several times. I put this code aside, solved part 1, and realized this was exactly what I needed for part two, so I simply reworded `score` into `rating` and it worked out of the box.
--- 2024/10 - Hoof It/part1.py
+++ 2024/10 - Hoof It/part1.py
@@ -1,104 +1,17 @@
-from enum import Enum
-from copy import deepcopy
+from collections import defaultdict
 
 
-class Dir(Enum):
-    N = 0
-    E = 1
-    S = 2
-    W = 3
+def explore_rec(z_map, positions, level):
+    if level == 10:
+        return len(positions)
 
-    def cw(self):
-        return Dir((self.value + 1) % 4)
-
-    def ccw(self):
-        return Dir((self.value - 1) % 4)
-
-    def move(self):
-        return ((-1, 0), (0, 1), (1, 0), (0, -1))[self.value]
-
-    def __str__(self):
-        return ('^', '>', 'v', '<')[self.value]
-
-
-class Pos(tuple):
-    def __new__(cls, i, j, grid_size):
-        return super(Pos, cls).__new__(cls, (i, j))
-
-    def __init__(self, i, j, grid_size):
-        self.grid_size = grid_size
-
-    def __add__(self, d):
-        di, dj = d.move()
-        i = self[0] + di
-        j = self[1] + dj
-        if i in [-1, self.grid_size[0]] or j in [-1, self.grid_size[1]]:
-            return None
-        return Pos(i, j, self.grid_size)
-
-    def next(self):
-        if self[1] == self.grid_size[1] - 1:
-            if self[0] == self.grid_size[0] - 1:
-                return None
-            else:
-                return Pos(self[0] + 1, 0, self.grid_size)
-        else:
-            return Pos(self[0], self[1] + 1, self.grid_size)
-
-
-class Trail:
-    def __init__(self, pos):
-        self.path = {k: set() for k in range(10)}
-        self.path[0].add(pos)
-
-    def find_path(self, next_z, pos):
-        global z_map
-        for d in [Dir.N, Dir.E, Dir.S, Dir.W]:
-            next_pos = pos + d
-            if next_pos and get_z(next_pos) == next_z:
-                self.path[next_z].add(next_pos)
-
-    # def print(self):
-    #     global z_map
-    #     grid = deepcopy(z_map)
-    #     for lvl in self.path.keys():
-    #         for pos in self.path[lvl]:
-    #             grid[pos[0]][pos[1]] = f'\033[31m{lvl}\033[0m'
-    #     print('-' * len(grid[0]))
-    #     print(''.join([''.join([c for c in l]) + '\n' for l in grid])[:-1])
-    #     print(f'score: {self.score()}')
-
-
-    def score(self):
-        return len(self.path[9])
-
-
-def get_z(pos):
-    global z_map
-    try:
-        return int(z_map[pos[0]][pos[1]])
-    except ValueError:
-        return -1
+    new_pos = set(p + d for p in positions for d in [-1j, 1, 1j, -1] if z_map[p + d] == level)
+    return explore_rec(z_map, new_pos, level + 1)
 
 
 def main(data):
-    grid_size = (len(data), len(data[0]))
-    global z_map
-    z_map = [[c for c in l] for l in data]
-
-    trails = []
-    cursor = Pos(0, 0, grid_size)
-    while cursor is not None:
-        if get_z(cursor) == 0:
-            trail = Trail(cursor)
-            for lvl in trail.path.keys():
-                for pos in trail.path[lvl]:
-                    trail.find_path(lvl + 1, pos)
-            # trail.print()
-            trails.append(trail)
-        cursor = cursor.next()
-
-    return sum([t.score() for t in trails])
+    z_map = defaultdict(int) | {j + 1j * i: int(c) for i, l in enumerate(data) for j, c in enumerate(l) if c.isdigit()}
+    return sum(explore_rec(z_map, [p], 1) for p in list(z_map) if z_map[p] == 0)
 
 
 if __name__ == '__main__':
@@ -106,7 +19,7 @@ if __name__ == '__main__':
     import time
 
     tic = time.time()
-    file = 'input.txt' if sys.argv[1:] else 'example5.txt'
+    file = 'input.txt' if sys.argv[1:] else 'example.txt'
     with open(file, 'r') as f:
         data = f.read().splitlines()
     result = main(data)
