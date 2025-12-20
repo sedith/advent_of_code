1fc7409a31a8c00510777d4c874e8206c05fdbea
sedith <martin.jacquet@posteo.net>
2025-12-21 00:15:53 +0100

    2025 09 p1

---
+++ 2025/09 - Movie Theater/README.md
@@ -0,0 +1,9 @@
+# Advent of code 2025 - Day 09: Movie Theater
+
+https://adventofcode.com/2025/day/9
+
+## Part 1
+
+Very much the same approach as 2025/08. Just discovered that `max` can also take a key function, quite convenient here.
+
+## Part 2
---
+++ 2025/09 - Movie Theater/part1.py
@@ -0,0 +1,21 @@
+def area(p1p2):
+    p1 = p1p2[0] ; p2 = p1p2[1]
+    return (abs(p1[0] - p2[0]) + 1 ) * (abs(p1[1] - p2[1]) + 1)
+
+def main(data):
+    points = [tuple(map(int, l.split(','))) for l in data]
+    return area(max(((p1, p2) for i1, p1 in enumerate(points) for i2, p2 in enumerate(points) if i1 < i2), key=area))
+
+
+if __name__ == '__main__':
+    import sys
+    import time
+
+    tic = time.time()
+    file = 'input.txt' if sys.argv[1:] else 'example.txt'
+    with open(file, 'r') as f:
+        data = f.read().splitlines()
+    result = main(data)
+    toc = time.time()
+    print(f'result   : {result}')
+    print(f'time [s] : {toc - tic:.5f}')
